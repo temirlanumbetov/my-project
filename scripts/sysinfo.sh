64049839376634d634e05497ff8a6caca47f1905
Temirlan <temirlanumbetov@gmail.com>
2026-05-10 22:26:40 +0500

    script output

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -3,9 +3,5 @@
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 RAM=$(free -m | awk '/Mem:/ {print $3}')
 
-cat <<EOF > report.html
-CPU: $CPU%
-RAM: $RAM MB
-EOF
-
-echo "Report generated: $(pwd)/report.html"
+echo "CPU: $CPU%"
+echo "RAM: $RAM MB"
